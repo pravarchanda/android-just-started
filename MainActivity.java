e7aeae0658759718b9169857859b99332a975cbb
pravar chanda <pravarchandamsd@gmail.com>
2016-06-17 00:18:09 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,8 +1,8 @@
-package com.example.pravar.myapplication;
+package com.example.pravar.registrationpage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,37 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"oncreate state",Toast.LENGTH_LONG).show();
     }
 
-    @Override
-    protected void onStart() {
-        Toast.makeText(this,"start",Toast.LENGTH_LONG).show();
-        super.onStart();
-    }
-
-    @Override
-    protected void onPause() {
-        Toast.makeText(this,"pause",Toast.LENGTH_LONG).show();
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        Toast.makeText(this,"resume",Toast.LENGTH_LONG).show();
-        super.onResume();
-    }
-
-    @Override
-    protected void onStop() {
-        Toast.makeText(this,"stop",Toast.LENGTH_LONG).show();
-        super.onStop();
-    }
-
-    @Override
-
-    protected void onDestroy() {
-        Toast.makeText(this,"destroy",Toast.LENGTH_LONG).show();
-        super.onDestroy();
+    public void nextpage(View view)
+    {
+        setContentView(R.layout.after_registration);
     }
 }
